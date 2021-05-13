07bf62baa9ceeb39d75b520a6dafaa353dc5104c
loucass003 <loucass003@gmail.com>
2021-05-13 17:17:15 +0200

    Test lint

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    'jest/globals': true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'jest'],
+  rules: {
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error',
+
+    'react/jsx-filename-extension': [0],
+    'import/extensions': 'off',
+    'no-use-before-define': 'off',
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
+  },
+};
